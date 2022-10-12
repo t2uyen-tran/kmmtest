03df458328386d6c4f7fd3af41c953e8c4c941bd
runner <runner@Mac-1665552495999.local>
2022-10-12 05:31:42 +0000

    KMM SPM package release for 0.1.2

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://api.github.com/repos/t2uyen-tran/kmmtest/releases/assets/80726061.zip"
-let remoteKotlinChecksum = "0465a8f7f3ea4da55644feb676508da960969af772c45ec96f9ab72aef0fa648"
+let remoteKotlinUrl = "https://api.github.com/repos/t2uyen-tran/kmmtest/releases/assets/80728272.zip"
+let remoteKotlinChecksum = "d813b8c184f692fe1fe0aea42be2a1abe15dcae827a6c0d224a077bd36e39dba"
 let packageName = "shared"
 
 let package = Package(
